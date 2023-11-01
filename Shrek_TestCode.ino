9ab3732c0e286e036c1ae470b6121a6d219a6082
Shelly202 <149627578+Shelly202@users.noreply.github.com>
2023-11-01 16:14:54 -0500

    Add files via upload

---
+++ Shrek_TestCode.ino
@@ -0,0 +1,24 @@
+// C++ code
+//
+
+#include <Servo.h>
+//taken from Prof code on Canvas slides
+
+Servo servo; //create servo variable
+const int buttonPin = 2; //create buttonpin
+
+void setup()
+{
+  servo.attach(12); // attach servo to pin 12
+  pinMode(buttonPin, INPUT);
+}
+
+void loop()
+{
+  //read if buttonPin
+  if (digitalRead(buttonPin) == LOW) { 
+    servo.write(180); //move servo 180
+  } else {
+    servo.write(0); //do not move servo/move servo 0 
+  }
+}
